5f93ed431bc74b7ba6c075b1a40e42836462c6c9
Matheus <matheusdevcj@gmail.com>
2024-10-06 17:03:31 -0300

    Rollback

---
+++ CadastroDeProdutosView/Features/Commons/CalculoDeCustoEMarkupParaPrecoDaVenda.cs
@@ -0,0 +1,20 @@
+﻿using DevExpress.XtraEditors;
+
+namespace CadastroDeProdutosView.Features.Commons
+{
+    public static class CalculoDeCustoEMarkupParaPrecoVenda
+    {
+        public static void CalcularPrecoVenda(decimal custo, decimal markup, TextEdit precoVendaTextEdit)
+        {
+            if (custo >= 0 && markup >= 0)
+            {
+                var precoVenda = custo *(1 + (markup / 100));
+                precoVendaTextEdit.Text = precoVenda.ToString("F2");
+            }
+            else
+            {
+                precoVendaTextEdit.Text = string.Empty;
+            }
+        }
+    }
+}
---
+++ CadastroDeProdutosView/Features/Commons/Services/ProdutoService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CadastroDeProdutosView.Features.Commons.Services
+{
+    public static class ProdutoService
+    {
+        public static List<Produto> ObterProdutosAtualizados()
+        {
+            var conexao = new ConexaoProdutosFirebird();
+            return conexao.ListarProdutos(incluirDesativados: false);
+        }
+    }
+}
