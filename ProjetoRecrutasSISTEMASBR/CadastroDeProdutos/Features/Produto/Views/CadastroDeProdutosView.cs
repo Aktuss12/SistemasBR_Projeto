9789276375f79cc1bdc7eb4fb23b314b265cc84d
Matheus <matheusdevcj@gmail.com>
2024-10-07 06:23:15 -0300

    Testes

--- ProjetoRecrutasSISTEMASBR/CadastroDeProdutos/Features/Produto/Views/CadastroDeProdutosView.cs
+++ ProjetoRecrutasSISTEMASBR/CadastroDeProdutos/Features/Produto/Views/CadastroDeProdutosView.cs
@@ -1,48 +1,46 @@
-﻿using System;
-using System.Drawing;
-using System.Globalization;
-using System.Linq;
-using System.Windows.Forms;
-using CadastroDeProdutosView.Features.Commons;
+﻿using CadastroDeProdutosView.Features.Commons;
 using CadastroDeProdutosView.Features.Commons.Services;
 using CadastroDeProdutosView.Features.Produto.Enums;
 using DevExpress.XtraBars;
 using DevExpress.XtraEditors;
+using System;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Windows.Forms;
 
 namespace CadastroDeProdutosView.Features.Produto.Views
 {
     public partial class CadastroDeProdutosView : Form
     {
         private readonly ConexaoProdutosFirebird _conexao;
-
         private readonly int? _produtoId;
-        private byte[] _imagemDoProduto = null!;
+        private byte[]? _imagemDoProduto;
 
         public CadastroDeProdutosView(int produtoId = 0)
         {
             InitializeComponent();
-            InitializeLookUpEdit();
-            LimitadorDeCaracteres();
-
+            InitializeComponents();
             _produtoId = produtoId > 0 ? produtoId : null;
             _conexao = new ConexaoProdutosFirebird();
+
             if (_produtoId.HasValue) CarregarProduto(_produtoId.Value);
+        }
+
+        private void InitializeComponents()
+        {
+            InitializeLookUpEdit();
+            LimitadorDeCaracteres();
         }
 
         private void InitializeLookUpEdit()
         {
-            unidadeDeMedidaLookUpEdit.
-                PreencherLookUpEditComOValorDoEnum<UnidadeDeMedidaView.UnidadeDeMedida>();
-            categoriaDeProdutosLookUpEdit
-                .PreencherLookUpEditComOValorDoEnum<CategoriaDoProdutoView.CategoriaDeProdutos>();
-            marcaLookUpEdit.
-                PreencherLookUpEditComOValorDoEnum<MarcaDoProdutoView.MarcaDoProduto>();
-            origemDaMercadoriaLookUpEdit
-                .PreencherLookUpEditComOValorDoEnum<OrigemDaMercadoriaView.OrigemDaMercadoria>();
-            situacaoTributariaLookUpEdit
-                .PreencherLookUpEditComOValorDoEnum<SituacaoTributariaView.SituacaoTributaria>();
-            naturezaDaOperacaoLookUpEdit
-                .PreencherLookUpEditComOValorDoEnum<NaturezaDaOperacaoView.NaturezaDaOperacao>();
+            unidadeDeMedidaLookUpEdit.PreencherLookUpEditComOValorDoEnum<UnidadeDeMedidaView.UnidadeDeMedida>();
+            categoriaDeProdutosLookUpEdit.PreencherLookUpEditComOValorDoEnum<CategoriaDoProdutoView.CategoriaDeProdutos>();
+            marcaLookUpEdit.PreencherLookUpEditComOValorDoEnum<MarcaDoProdutoView.MarcaDoProduto>();
+            origemDaMercadoriaLookUpEdit.PreencherLookUpEditComOValorDoEnum<OrigemDaMercadoriaView.OrigemDaMercadoria>();
+            situacaoTributariaLookUpEdit.PreencherLookUpEditComOValorDoEnum<SituacaoTributariaView.SituacaoTributaria>();
+            naturezaDaOperacaoLookUpEdit.PreencherLookUpEditComOValorDoEnum<NaturezaDaOperacaoView.NaturezaDaOperacao>();
         }
 
         private void LimitadorDeCaracteres()
@@ -90,44 +88,46 @@ namespace CadastroDeProdutosView.Features.Produto.Views
             try
             {
                 var produto = CriarProduto();
-
                 if (_produtoId.HasValue)
                 {
                     produto.Id = _produtoId.Value;
                     _conexao.AlterarProduto(produto);
-                    XtraMessageBox.Show("Produto alterado com sucesso");
+                    ShowMessage("Produto alterado com sucesso");
                     Close();
                 }
                 else
                 {
-                    var novoProdutoid = _conexao.InserirProduto(produto);
-                    XtraMessageBox.Show("Produto cadastrado com sucesso");
-                    produto.Id = novoProdutoid;
+                    var novoProdutoId = _conexao.InserirProduto(produto);
+                    ShowMessage("Produto cadastrado com sucesso");
+                    produto.Id = novoProdutoId;
                 }
 
                 LimparLookUpEditsETextEdits();
-
                 ProdutosAtualizados?.Invoke();
             }
             catch (Exception ex)
             {
-                XtraMessageBox.Show($"Erro ao salvar o produto: {ex.Message}");
+                ShowMessage($"Erro ao salvar o produto: {ex.Message}");
             }
+        }
+
+        private void ShowMessage(string message)
+        {
+            XtraMessageBox.Show(message);
         }
 
         private bool ValidarCampos()
         {
-            if (!string.IsNullOrWhiteSpace(codigoDeBarrasTextEdit.Text))
-                if (!_conexao.ValidarCodigoDeBarras(codigoDeBarrasTextEdit.Text))
-                {
-                    XtraMessageBox.Show("O código de barras não é um tipo EAN-13");
-                    codigodebarrasLabelControl.Text = "Código de Barras: <color=red>*</color>";
-                    codigodebarrasLabelControl.AllowHtmlString = true;
-                    return false;
-                }
+            if (!string.IsNullOrWhiteSpace(codigoDeBarrasTextEdit.Text) &&
+                !_conexao.ValidarCodigoDeBarras(codigoDeBarrasTextEdit.Text))
+            {
+                ShowMessage("O código de barras não é um tipo EAN-13");
+                codigodebarrasLabelControl.Text = "Código de Barras: <color=red>*</color>";
+                codigodebarrasLabelControl.AllowHtmlString = true;
+                return false;
+            }
 
-            codigodebarrasLabelControl.Text = "Codigo de Barras:";
-            codigodebarrasLabelControl.AllowHtmlString = false;
+            ResetCodigoDeBarrasLabel();
 
             if (ValidacaoDeCamposObrigatorios.ValidacaoParaCamposObrigatorios(
                     nomeTextEdit,
@@ -139,14 +139,22 @@ namespace CadastroDeProdutosView.Features.Produto.Views
                     estoqueLabelControl,
                     precoDaVendaLabelControl,
                     unidadeDeMedidaLabelControl,
-                    categoriaLabelControl)) return true;
-            XtraMessageBox.Show("Todos os campos obrigatórios devem ser preenchidos!");
+                    categoriaLabelControl))
+                return true;
+
+            ShowMessage("Todos os campos obrigatórios devem ser preenchidos!");
             return false;
+        }
+
+        private void ResetCodigoDeBarrasLabel()
+        {
+            codigodebarrasLabelControl.Text = "Codigo de Barras:";
+            codigodebarrasLabelControl.AllowHtmlString = false;
         }
 
         private Commons.Produto CriarProduto()
         {
-            return new Commons.Produto()
+            return new Commons.Produto
             {
                 Nome = nomeTextEdit.Text,
                 Categoria = categoriaDeProdutosLookUpEdit.EditValue?.ToString(),
@@ -155,35 +163,33 @@ namespace CadastroDeProdutosView.Features.Produto.Views
                 UnidadeDeMedida = unidadeDeMedidaLookUpEdit.EditValue?.ToString(),
                 Estoque = int.Parse(estoqueTextEdit.Text),
                 Marca = marcaLookUpEdit.EditValue?.ToString(),
-                Custo = ConversorParaDecimal.ParseDecimal(custoTextEdit.Text),
-                Markup = ConversorParaDecimal.ParseDecimal(markupTextEdit.Text),
-                PrecoDaVenda = ConversorParaDecimal.ParseDecimal(precoVendaTextEdit.Text),
+                Custo = ConversaoUtil.ConversaoParaDecimal(custoTextEdit.Text),
+                Markup = ConversaoUtil.ConversaoParaDecimal(markupTextEdit.Text),
+                PrecoDaVenda = ConversaoUtil.ConversaoParaDecimal(precoVendaTextEdit.Text),
                 Imagem = _imagemDoProduto,
+
                 InformacoesFiscais = new InformacoesFiscais
                 {
                     OrigemDaMercadoria = origemDaMercadoriaLookUpEdit.EditValue?.ToString(),
                     SituacaoTributaria = situacaoTributariaLookUpEdit.EditValue?.ToString(),
                     NaturezaDaOperacao = naturezaDaOperacaoLookUpEdit.EditValue?.ToString(),
                     Ncm = ncmTextEdit.Text,
-                    AliquotaDeIcms = ConversorParaDecimal.ParseDecimal(aliquotaDeIcmsTextEdit.Text),
-                    ReducaoDeCalculo = ConversorParaDecimal.ParseDecimal(reducaoDeCalculoIcmsTextEdit.Text)
+                    AliquotaDeIcms = ConversaoUtil.ConversaoParaDecimal(aliquotaDeIcmsTextEdit.Text),
+                    ReducaoDeCalculo = ConversaoUtil.ConversaoParaDecimal(reducaoDeCalculoIcmsTextEdit.Text)
                 }
             };
         }
-
 
         public void CarregarProduto(int idProduto)
         {
             try
             {
                 var produto = _conexao.CarregarProduto(idProduto);
-                if (produto == null!) return;
-
                 PreencherFormularioComProduto(produto);
             }
             catch (Exception ex)
             {
-                XtraMessageBox.Show($"Erro ao carregar o produto: {ex.Message}");
+                ShowMessage($"Erro ao carregar o produto: {ex.Message}");
             }
         }
 
@@ -203,85 +209,57 @@ namespace CadastroDeProdutosView.Features.Produto.Views
             situacaoTributariaLookUpEdit.EditValue = produto.InformacoesFiscais.SituacaoTributaria;
             naturezaDaOperacaoLookUpEdit.EditValue = produto.InformacoesFiscais.NaturezaDaOperacao;
             ncmTextEdit.Text = produto.InformacoesFiscais.Ncm;
-            aliquotaDeIcmsTextEdit.Text =
-                produto.InformacoesFiscais.AliquotaDeIcms.ToString(CultureInfo.InvariantCulture);
-            reducaoDeCalculoIcmsTextEdit.Text =
-                produto.InformacoesFiscais.ReducaoDeCalculo.ToString(CultureInfo.InvariantCulture);
-
-            if (produto.Imagem == null) return;
-            imagemDoProdutoPictureEdit.Image = _conexao.ConverterBytesParaImagem(produto.Imagem);
+            aliquotaDeIcmsTextEdit.Text = produto.InformacoesFiscais.AliquotaDeIcms.ToString(CultureInfo.InvariantCulture);
+            reducaoDeCalculoIcmsTextEdit.Text = produto.InformacoesFiscais.ReducaoDeCalculo.ToString(CultureInfo.InvariantCulture); 
             _imagemDoProduto = produto.Imagem;
+            if (produto.Imagem != null) imagemDoProdutoPictureEdit.Image = Image.FromStream(new MemoryStream(produto.Imagem));
         }
 
-        private void adicionarImagemButton_Click(object sender, EventArgs e)
+        private void AdicionarImagemButton_Click(object sender, EventArgs e)
         {
-            using var abrirExploradoDeArquivo = new OpenFileDialog();
-            abrirExploradoDeArquivo.Filter =
-                "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif;*.tiff;*.tif;*.webp;*.heic;*.svg;*.cr2;*.nef;*.arw";
+            using var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+            if (openFileDialog.ShowDialog() != DialogResult.OK) return;
 
-            if (abrirExploradoDeArquivo.ShowDialog() != DialogResult.OK) return;
+            _imagemDoProduto = File.ReadAllBytes(openFileDialog.FileName);
+            imagemDoProdutoPictureEdit.Image = Image.FromFile(openFileDialog.FileName);
+        }
 
-            var caminhoDaImagem = abrirExploradoDeArquivo.FileName;
-            _imagemDoProduto = _conexao.ConverterImagemParaBytes(caminhoDaImagem, 190, 241);
-
-            using var imagemOriginal = Image.FromFile(caminhoDaImagem);
-            imagemDoProdutoPictureEdit.Image = new Bitmap(imagemOriginal, new Size(190, 241));
+        private void ExcluirImagem(object sender, EventArgs e)
+        {
+            _imagemDoProduto = null;
+            imagemDoProdutoPictureEdit.Image = null;
         }
 
         private void codigoDeBarrasButton_Click(object sender, EventArgs e)
         {
-            codigoDeBarrasTextEdit.EditValue = _conexao.GerarCodigoDeBarras();
+            var gerarCodigoDeBarras = CalculadorDeCodigoDeBarras.GerarCodigoDeBarrasEAN13();
+            if (!string.IsNullOrWhiteSpace(gerarCodigoDeBarras))
+            {
+                codigoDeBarrasTextEdit.EditValue = gerarCodigoDeBarras;
+            }
         }
 
-        private void custoTextEdit_EditValueChanged(object sender, EventArgs e)
+        private void alterarBancoDeDados(object sender, EventArgs e)
         {
-            AtualizarPrecoVenda();
+
         }
 
-        private void markupTextEdit_EditValueChanged_1(object sender, EventArgs e)
+        private void limparCodigoDeBarras(object sender, EventArgs e)
         {
-            AtualizarPrecoVenda();
         }
 
-
-        private void AtualizarPrecoVenda()
+        private void AtualizarPrecoVenda(object sender, EventArgs e)
         {
-            if (!decimal.TryParse(custoTextEdit.Text, out var custo) ||
-                !decimal.TryParse(markupTextEdit.Text, out var markup)) return;
-            var precoVenda = _conexao.CalcularPrecoVenda(custo, markup);
-            precoVendaTextEdit.Text = precoVenda.ToString("N2");
+            if (decimal.TryParse(custoTextEdit.Text, out var custo) &&
+                decimal.TryParse(markupTextEdit.Text, out var markup))
+            {
+                precoVendaTextEdit.Text = (custo + (custo * (markup / 100))).ToString(CultureInfo.InvariantCulture);
+            }
         }
 
         private void pesquisarProdutoButtomItem_ItemClick(object sender, ItemClickEventArgs e)
         {
-            var pesquisaForm = Application.OpenForms.OfType<PesquisaDeProdutosView>().FirstOrDefault();
-            if (pesquisaForm != null)
-            {
-                pesquisaForm.Activate();
-                return;
-            }
-
-            var pesquisarProdutos = new PesquisaDeProdutosView();
-            pesquisarProdutos.ShowDialog();
-        }
-
-        private void alterarBancoDeDadosButton_Click_1(object sender, EventArgs e)
-        {
-            var alterarBancoDeDados = new ConfigurarCaminhoDoBancoDeDadosView();
-            alterarBancoDeDados.ShowDialog();
-        }
-
-        private void excluirImagemButton_Click(object sender, EventArgs e)
-        {
-            imagemDoProdutoPictureEdit.Image = null;
-            _imagemDoProduto = null!;
-        }
-
-        private void codigoDeBarrasTextEdit_EditValueChanged(object sender, EventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(codigoDeBarrasTextEdit.Text) || codigoDeBarrasTextEdit.Text != "0") return;
-            codigoDeBarrasTextEdit.EditValue = null;
-            codigoDeBarrasTextEdit.Refresh();
         }
     }
 }
